6fcce14fab8c9bb539ea9b28b616880b7a2d1f48
taeyangcode <taeyangcode@gmail.com>
2024-05-13 01:07:20 -0700

    Audio Player initial implementation

--- src/audio.rs
+++ src/audio.rs
@@ -1,3 +1,12 @@
+use std::{
+    ops::Deref,
+    sync::{Arc, Mutex},
+};
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    Device, Host, SupportedStreamConfig,
+};
 use strum::AsRefStr;
 
 #[derive(AsRefStr)]
@@ -45,4 +54,61 @@ const STT_SAMPLE_RATE: usize = 44100;
 const STT_SILENCE_THRESHOLD: usize = 1500;
 const STT_WAIT_SEC: std::time::Duration = std::time::Duration::from_secs(5);
 
-struct AudioRecorder {}
+pub struct AudioDevice {
+    host: Host,
+    device: Device,
+    input_config: SupportedStreamConfig,
+    buffer: Vec<f32>,
+}
+
+impl AudioDevice {
+    pub fn new() -> Self {
+        let host: Host = cpal::default_host();
+        let device: Device = host.default_input_device().unwrap();
+        let supported_config = device
+            .supported_input_configs()
+            .unwrap()
+            .next()
+            .unwrap()
+            .with_max_sample_rate();
+
+        println!("{}", device.name().unwrap());
+
+        Self {
+            host,
+            device,
+            input_config: supported_config,
+            buffer: vec![],
+        }
+    }
+
+    pub fn record_audio(&self) {
+        let buffer: Arc<Mutex<Option<Vec<f32>>>> = Arc::new(Mutex::new(Some(vec![])));
+        let stream_buffer = buffer.clone();
+
+        let stream = self
+            .device
+            .build_input_stream(
+                &self.input_config.config(),
+                move |data: &[f32], _: &cpal::InputCallbackInfo| {
+                    if let Ok(mut guard) = stream_buffer.try_lock() {
+                        if let Some(buffer) = guard.as_mut() {
+                            for &data in data.iter() {
+                                buffer.push(data);
+                            }
+                        }
+                    }
+                },
+                move |error| {
+                    dbg!(error);
+                },
+                None,
+            )
+            .unwrap();
+
+        stream.play().unwrap();
+
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        drop(stream);
+    }
+}
